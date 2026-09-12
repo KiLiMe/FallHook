1f13dbca25ea210689cc202fe7a13472ff4fe55a
KiLiMe <80700107+KiLiMe@users.noreply.github.com>
2026-09-12 22:05:25 +0800

    Revert "fix: add forward declaration for ParseFileImpl"
    This reverts commit 3c714f9ac59df943a8e51d917ce11941b4cfb1c3.

--- src/XmlTranslationParser.cpp
+++ src/XmlTranslationParser.cpp
@@ -275,12 +275,6 @@ namespace
 	}
 }
 
-// 前向声明
-namespace
-{
-	XmlParseResult ParseFileImpl(const std::filesystem::path& path, std::string_view recordFilter);
-}
-
 namespace XmlTranslationParser
 {
 	XmlParseResult ParseFile(const std::filesystem::path& path)
